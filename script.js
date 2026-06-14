9d2a32e723ea93a5b2c6b53455ce68e26cc3aab0
Kaushal Shakya <kkagrilengg@gmail.com>
2026-06-14 13:36:45 +0530

    Updated script.js with dark mode

--- script.js
+++ script.js
@@ -570,3 +570,36 @@ spacing.innerHTML=`
 }
 
 }
+function toggleDarkMode() {
+
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+
+localStorage.setItem("theme","dark");
+
+document.getElementById("darkModeBtn").innerHTML="☀️ Light Mode";
+
+}
+
+else{
+
+localStorage.setItem("theme","light");
+
+document.getElementById("darkModeBtn").innerHTML="🌙 Dark Mode";
+
+}
+
+}
+
+window.onload=function(){
+
+if(localStorage.getItem("theme")=="dark"){
+
+document.body.classList.add("dark-mode");
+
+document.getElementById("darkModeBtn").innerHTML="☀️ Light Mode";
+
+}
+
+}
